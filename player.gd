21a92cca5182b5d4262de21e5fb0629ec6aafcef
evolentDelight <evolentDelight@outlook.com>
2025-06-09 17:01:18 -0700

    Add player's velocity and position code

--- player.gd
+++ player.gd
@@ -4,10 +4,28 @@ extends Area2D
 var screen_size # Size of the game window
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta: float):
+	var velocity = Vector2.ZERO # The player's movement vector
+	
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size) #position.clamp(,) is to prevent the player unit from leaving the screen
