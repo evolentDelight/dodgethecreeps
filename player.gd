013e53b105c1ef38323c57ad418de8bc9e998ac8
evolentDelight <evolentDelight@outlook.com>
2025-06-11 20:07:24 -0700

    Added Animation to Player Sprite

--- player.gd
+++ player.gd
@@ -6,7 +6,7 @@ var screen_size # Size of the game window
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
-
+	hide() #Hides the Player Node Type: Area2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float):
@@ -20,7 +20,7 @@ func _process(delta: float):
 		velocity.y += 1
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
-	
+
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
@@ -29,3 +29,11 @@ func _process(delta: float):
 
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size) #position.clamp(,) is to prevent the player unit from leaving the screen
+
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		#$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
