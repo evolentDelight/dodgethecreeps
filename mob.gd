9e25ba2b1513e4a174ba573342f4c607e1d782d9
evolentDelight <evolentDelight@outlook.com>
2025-06-13 15:57:10 -0700

    Delete unused function _process

--- mob.gd
+++ mob.gd
@@ -12,11 +12,5 @@ func _ready() -> void:
 	else:
 		$CollisionShape_Fly.disabled = true
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
