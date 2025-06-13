1e08a16f6070327b3634950d57121e6dc8033c32
evolentDelight <evolentDelight@outlook.com>
2025-06-13 15:54:34 -0700

    Add CollisionShape2D for fly animation
    The fly animation has longer legs that requires a different collision shape.
    Script also reflects the change with collision.disabled conditions

---
+++ mob.gd
@@ -0,0 +1,22 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
+	var mob_type = mob_types.pick_random()
+	$AnimatedSprite2D.animation = mob_type
+	$AnimatedSprite2D.play()
+	if mob_type == "fly":
+		$CollisionShape_Fly.disabled = false
+	else:
+		$CollisionShape_Fly.disabled = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
