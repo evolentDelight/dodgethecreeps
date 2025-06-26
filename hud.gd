9235741aca7b90af0ac78947ec3b705561c7d5e0
evolentDelight <evolentDelight@outlook.com>
2025-06-26 16:20:35 -0700

    Remove debug code
    remove print node's tree

--- hud.gd
+++ hud.gd
@@ -18,7 +18,7 @@ func show_game_over():
 	
 	# Make a one-shot timer and wait for it to finish
 	await get_tree().create_timer(1.0).timeout
-	print(get_tree_string_pretty()) # Debugging purpose
+	#print(get_tree_string_pretty()) # Debugging purpose
 	
 	$StartButton.show()
 
